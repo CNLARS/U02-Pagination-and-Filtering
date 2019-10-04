b1a366f4878c3c129e64e035a18a14588164c602
CNLARS <CNLARS@users.noreply.github.com>
2019-10-04 14:38:40 -0700

    7.7 Edition - paginationInProgress

--- js/script.js
+++ js/script.js
@@ -5,31 +5,14 @@ FSJS project 2 - List Filter and Pagination
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-
-   Pro Tip: Log out the variable storing the list to ensure it equals the list of li items and not the container of the li elements.
-***/
 const studentList = document.querySelectorAll('.student-item');
 const pageDisplay = 10;
-
-
 
 /***
    Create the `showPage` function to hide all of the items in the
    list except for the ten you want to show.
 
    Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
      - Remember that the first student has an index of 0.
      - Remember that a function `parameter` goes in the parens when
        you initially define the function, and it acts as a variable
@@ -53,59 +36,57 @@ for (let i = 0; i < list.length; i += 1)  {
 
 showPage(studentList, 1);
 
-
-
 /***
    Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
-function appendPageLinks (list) {
+const appendPageLinks => (list) {
 const div = document.createElement('div');
 const ul = document.getElementsByTagName('ul');
-const li = document.getElementsByTagName('li');
+const listItem = document.getElementsByTagName('li');
 
-div.className = 'pagination'
-getElementsByClassName('.page').appendChild(div);
-getElementsByClassName('pagination').appendChild(ul);
-ul.appendChild(li); //or li[i];?
+/* 1. Determine how many pages are needed for the list by dividing the
+total number of list items by the max number of items per page*/
+while (i = 0; i < listItem.length; i ++) {
+  let page = (listItem[i] / Math.max(pageDisplay));
 
-if (i = 0; i < list.length; i ++) {
-  let page = (list[i] / Math.max(pageDisplay);
   for (let i = 0; i < page.length; i++) {
-
+if (i <= page.length) {
+  page[i].style.display = 'block';
+} else {
+  page[i].style.display = 'none';
+}
+console.log(page)[i];
   }
 }
-for (i = 0; i < li.length; i ++) {
-li.createElement('a');
- li.addEventListener('click',() => {
+//2. Create a div, give it the “pagination” class, and append it to the .page div
+div.className = 'pagination';
+document.getElementsByClassName('.page')[0].appendChild(div);
 
+//3. Add a ul to the “pagination” div to store the pagination links
+document.getElementsByClassName('pagination').appendChild(ul);
+ul.appendChild(li); //or li[i];?
 
-});
+//4. for every page, add li and a tags with the page number text
+for (i = 0; i < page.length; i ++) {
+page[i] += listItem.a;
+page[i].textContent = page.textContent.number.parseInt();
+
+/*5. Add an event listener to each a tag. When they are clicked
+call the showPage function to display the appropriate page*/
+listItem.addEventListener('click', getElementsByTagName('a'), (showPage()) => {
+for (i = 0; i < showPage.length; i++) {
+  // if (){
+  //   showPage(studentList[i], )
+  // }
 }
 
-
-// if (){
-//   showPage(studentList[i], )
-// }
+}); // addEventListener bracket
+} // for bracket
 
 } // <---function bracket
 
-
-
 /*
-1. MAYBE? Determine how many pages are needed for the list by dividing the
-total number of list items by the max number of items per page
-
-
-2. DONE 67-68 Create a div, give it the “pagination” class, and append it to the .page div
-
-3. DONE 69 Add a ul to the “pagination” div to store the pagination links
-
-4. ??? for every page, add li and a tags with the page number text
-
-5. Add an event listener to each a tag. When they are clicked
-call the showPage function to display the appropriate page
-
 6. Loop over pagination links to remove active class from all links
 
 7. Add the active class to the link that was just clicked. You can identify that
@@ -133,10 +114,5 @@ clicked link using event.target
   //   </ul>
   // </div>
   //<!-- end pagination -->
-
-
-
-
-
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
