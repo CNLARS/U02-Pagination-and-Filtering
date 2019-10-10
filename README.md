771286d4acc47e139847a599b44c566ea736c85b
CNLARS <CNLARS@users.noreply.github.com>
2019-10-09 18:27:22 -0700

    Final Edition (Meets Expectations)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # U02-Pagination-and-Filtering
 Treehouse Techdegree U02 Project -  TalentPath Jumpstart SEA
+
+function showPage(studentList, page)
+  Calculation with a for loop using if/else to determine display of list[i].
+
+function appendPageLinks(list) c
+  Calculating how many pages to display '.student-item', creating pagination div and appending ul/li/a elements, with an addEventListener looping through links to shift '.active' on selected link when clicked.
+
+  Meets Expectations - will go for exceeds with Unit 04 Project.
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /******************************************
-Treehouse Techdegree:
+Treehouse Techdegree: CNLARS
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 const studentList = document.querySelectorAll('.student-item');
 const pageDisplay = 10;
-
+//showPage Math and for looped with an if/else style.display to determine list length.
 function showPage (list, page) {
   const startIndex = (page * pageDisplay) - pageDisplay;
   const endIndex = (page * pageDisplay) - 1;
@@ -19,29 +19,27 @@ for (let i = 0; i < list.length; i += 1)  {
     }
   }
 }
-
+//front page
 showPage(studentList, 1);
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
+
+
 function appendPageLinks (list) {
 /* 1. Determine how many pages are needed for the list by dividing the
 total number of list items by the max number of items per page*/
 let page = Math.ceil(studentList.length/pageDisplay);
 
-//2. Create a div, give it the “pagination” class, and append it to the .page div
+//2. div.pagination appending to .page div
 const div = document.createElement('div');
 div.className = 'pagination';
 const pageDiv = document.body.firstElementChild;
 pageDiv.appendChild(div);
 
-//3. Add a ul to the “pagination” div to store the pagination links
+//3. 'ul' added to div.pagination to store links.
 const ul = document.createElement('ul');
 const divPagination = document.body.firstElementChild.lastElementChild;
 divPagination.appendChild(ul);
 
-//4. for every page, add li and a tags with the page number text
+//4. for loop previous page Math appending child elements a tag and then li after defining innerText and href.
   for (i = 1; i <= page; i ++) {
     const li = document.createElement('li')
     const a = document.createElement('a');
@@ -50,27 +48,26 @@ divPagination.appendChild(ul);
     a.href = '#';
     li.appendChild(a);
     ul.appendChild(li);
-//Instructions say to add intial active class to 1st a tag.
+
+// *Instructions say to add intial active class to 1st a tag.*
   const activeFrontPage = document.querySelector('div.pagination a');
   activeFrontPage.className = 'active';
-/*5. Add an event listener to each a tag. When they are clicked
-call the showPage function to display the appropriate page*/
-//Defining a tags
-const pageLinks = document.querySelector('a');
 
-pageLinks.addEventListener('click', (e) => {
-  showPage(studentList, i);
-//6. Loop over pagination links to remove active class from all links
-/*7. +Add the active class to the link that was just clicked. You can identify that
-  clicked link using event.target*/
-  for  (let i = 0; i < divPagination.length; i ++) {
-  const links = querySelectorAll('a');
-    links[i].className.remove('active');
+//5. Re-defining page inside addEventListener before calling showPage()
+a.addEventListener('click', (e) => {
+  page = a.innerText;
+  showPage(studentList, page);
+/*6 + 7. Loop over pagination links to remove active class from all links
++ Add the active class to the link that was just clicked.*/
+  const links = document.querySelectorAll('a');
+  for  (let i = 0; i < links.length; i ++) {
+    links[i].classList.remove('active');
     const selection = e.target;
-      selection.className.add('active');
+      selection.classList.add('active');
   }
     });
   }
 }
 appendPageLinks();
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+//Concise comments provided,question(s) submitted with project; personal comment: worth all the hours of study.  Ready for U03!
